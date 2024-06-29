54d060e58c05ec508bc330dc127d91f65afcce3d
StalynG <153782308+StalynG@users.noreply.github.com>
2024-06-29 18:18:25 -0500

    Filament Install
    Installation: Livewire, Filament.
    
    Migrations: recipes, ingredients, categories, category_recipe.
    
    Models: Recipe, Category, Ingredient.

---
+++ app/Models/Recipe.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function categories(){
+        return $this->belongsToMany(Category::class,'category_recipe');
+    }
+
+    public function ingredients(){
+        return $this->hasMany(Ingredient::class,'id');
+    }
+
+    public function users(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+}
