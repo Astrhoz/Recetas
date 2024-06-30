63f8d7bcc9e929da556bd8ab66cfe98a7294a769
StalynG <153782308+StalynG@users.noreply.github.com>
2024-06-29 22:44:36 -0500

    CRUD
    Admin CRUD: Recipes, Categories

---
+++ app/Filament/Resources/RecipeResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RecipeResource\Pages;
+use App\Filament\Resources\RecipeResource\RelationManagers;
+use App\Models\Recipe;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RecipeResource extends Resource
+{
+    protected static ?string $model = Recipe::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\RichEditor::make('steps')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('amount_of_ingredientes')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\Textarea::make('tips')
+                    ->required()
+                    ->columnSpanFull(),
+                Forms\Components\FileUpload::make('images')
+                    ->required()
+                    ->directory('recipes'),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('users','name')
+                    ->required()
+                    ->searchable()
+                    ->preload(),
+                Forms\Components\Select::make('categories')
+                    ->required()
+                    ->multiple()
+                    ->relationship('categories', 'name')
+                    ->searchable()
+                    ->preload(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('amount_of_ingredientes')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('images')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRecipes::route('/'),
+            'create' => Pages\CreateRecipe::route('/create'),
+            'view' => Pages\ViewRecipe::route('/{record}'),
+            'edit' => Pages\EditRecipe::route('/{record}/edit'),
+        ];
+    }
+}
