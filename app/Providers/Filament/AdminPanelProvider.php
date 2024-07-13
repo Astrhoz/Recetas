aaac963530c7b4de61720a6b7aa22da624a3a7ab
StalynG <153782308+StalynG@users.noreply.github.com>
2024-07-13 18:38:15 -0500

    Widgets dashboard
    Se agregaron widgets para los dashboard de administrador y usuario

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,8 +36,9 @@ class AdminPanelProvider extends PanelProvider
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
+                \App\Filament\Widgets\AdminWidgetStats::class,
+                \App\Filament\Widgets\AdminWidgetChart::class,
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
