3d6e683cfbe63273de542c41c95ac6a80397a487
Dranskii <fernandobeltran.lab@gmail.com>
2024-08-05 10:32:58 -0500

    Endpoints de retorno archivos JSON

--- resources/views/components/recipe/recipe-card.blade.php
+++ resources/views/components/recipe/recipe-card.blade.php
@@ -7,7 +7,7 @@
         <p class="text-primary mb-4 font-extralight">
             {{ \Illuminate\Support\Str::limit($recipe->description, 100) }}
         </p>
-        <a href="#" class="inline-flex items-center gap-2 bg-secondary-100/80 text-secondary px-4 py-2 rounded-md hover:bg-secondary-900 hover:text-primary">
+        <a href="{{ route('recipe', $recipe->id) }}" class="inline-flex items-center gap-2 bg-secondary-100/80 text-secondary px-4 py-2 rounded-md hover:bg-secondary-900 hover:text-primary">
             Ver receta
         </a>
     </div>
